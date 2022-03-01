182fbb8108b455189183c57367374eb44938b686
Chris Smith <cdsmith@gmail.com>
2022-03-01 13:50:22 -0500

    Change rep of Dist.  Fixes #1

--- src/Dist.hs
+++ src/Dist.hs
@@ -20,38 +20,29 @@ where
 
 import Control.Applicative (liftA2)
 import Control.Monad (ap)
-import Data.Bifunctor (first)
+import Data.Bifunctor (bimap)
 import qualified Data.Map as Map
 import Data.Tuple (swap)
 import System.Random (randomRIO)
+import qualified Data.PQueue.Prio.Max as PQ
 
 -- | Probabilities are represented as rational numbers so we can ensure they add
 -- to 1.
 type Probability = Rational
 
 -- | A probability distribution of values.
-newtype Dist a = Dist {unDist :: [(Probability, a)]}
+data Dist a
+  = Certainly a
+  | Choice [(Probability, Dist a)]
   deriving (Functor, Show)
 
 instance Applicative Dist where
-  pure x = Dist [(1, x)]
+  pure = Certainly
   (<*>) = ap
 
 instance Monad Dist where
-  m >>= f = joinDist (fmap f m)
-
--- The join function for the 'Dist' monad.  This is a little tricky because it
--- needs to work properly for infinite distributions.
-joinDist :: Dist (Dist a) -> Dist a
-joinDist (Dist dist) =
-  Dist (flatten [[(p * q, x) | (q, x) <- dist'] | (p, Dist dist') <- dist])
-
-flatten :: [[a]] -> [a]
-flatten [] = []
-flatten (x : xs) = interleave x (flatten xs)
-  where
-    interleave [] ys = ys
-    interleave (z : zs) ys = z : interleave ys zs
+  Certainly x >>= f = f x
+  Choice xs >>= f = Choice $ map (fmap (>>= f)) xs
 
 instance Num a => Num (Dist a) where
   (+) = liftA2 (+)
@@ -72,19 +63,19 @@ instance Fractional a => Fractional (Dist a) where
 -- even distributions with finitely many outcomes, but infinitely many paths to
 -- reach those outcomes) will hang.
 simplify :: Ord a => Dist a -> Dist a
-simplify (Dist dist) =
-  Dist $ swap <$> Map.toList (Map.fromListWith (+) (swap <$> dist))
+simplify dist =
+  Choice $ fmap Certainly . swap <$> Map.toList (Map.fromListWith (+) (swap <$> possibilities dist))
 
 -- | A uniform distribution over a list of values.
 uniform :: [a] -> Dist a
-uniform xs = Dist [(recip n, x) | x <- xs]
+uniform xs = Choice [(recip n, Certainly x) | x <- xs]
   where
     n = toRational (length xs)
 
 -- | A Bernoulli distribution.  This gives True with probability @p@, and False
 -- otherwise.
 bernoulli :: Probability -> Dist Bool
-bernoulli p = Dist [(p, True), (1 - p, False)]
+bernoulli p = Choice [(p, Certainly True), (1 - p, Certainly False)]
 
 -- | Computes the probability of an event, represented by a predicate on values.
 --
@@ -92,13 +83,13 @@ bernoulli p = Dist [(p, True), (1 - p, False)]
 -- even distributions with finitely many outcomes, but infinitely many paths to
 -- reach those outcomes) will hang.
 probability :: (a -> Bool) -> Dist a -> Probability
-probability event = sum . fmap fst . filter (event . snd) . unDist
+probability event = sum . fmap fst . filter (event . snd) . possibilities
 
 -- | Like probability, but produces a lazy list of ever-improving ranges of
 -- probabilities.  This can be used on infinite distributions, for which the
 -- exact probability cannot be calculated.
 approxProbability :: (a -> Bool) -> Dist a -> [(Probability, Probability)]
-approxProbability event = go 0 1 . unDist
+approxProbability event = go 0 1 . possibilities
   where
     go p _ [] = [(p, p)]
     go p q ((q', x) : xs)
@@ -111,7 +102,7 @@ approxProbability event = go 0 1 . unDist
 -- even distributions with finitely many outcomes, but infinitely many paths to
 -- reach those outcomes) will hang.
 expectation :: Fractional a => Dist a -> a
-expectation (Dist dist) = sum [ fromRational p * x | (p, x) <- dist ]
+expectation dist = sum [ fromRational p * x | (p, x) <- possibilities dist ]
 
 -- | Computes the variance of a finite distribution.
 --
@@ -133,8 +124,15 @@ stddev dist = sqrt (variance dist)
 -- | Gives the list of all possibile values of a given probability distribution.
 -- This will often contain duplicate values, which can be removed using 'nub',
 -- 'Data.Set.fromList', etc.
-possibilities :: Dist a -> [a]
-possibilities = fmap snd . unDist
+possibilities :: Dist a -> [(Probability, a)]
+possibilities dist = go (PQ.singleton 1 dist)
+  where
+    go queue
+      | PQ.null queue = []
+      | otherwise = case PQ.deleteFindMax queue of
+        ((p, Certainly x), queue') -> (p, x) : go queue'
+        ((p, Choice xs), queue') ->
+          go (foldr (\(q, x) -> PQ.insert (p * q) x) queue' xs)
 
 -- | Produces the conditional probability distribution, assuming some event.
 --
@@ -142,25 +140,24 @@ possibilities = fmap snd . unDist
 -- even distributions with finitely many outcomes, but infinitely many paths to
 -- reach those outcomes) will hang.
 finiteConditional :: (a -> Bool) -> Dist a -> Dist a
-finiteConditional event dist = Dist (map (first (/ p_event)) filtered)
+finiteConditional event dist = Choice (map (bimap (/ p_event) Certainly) filtered)
   where
-    filtered = filter (event . snd) (unDist dist)
+    filtered = filter (event . snd) (possibilities dist)
     p_event = sum (map fst filtered)
 
 -- | Produces the conditional probability distribution, assuming some event.
 -- This function works for all distributions, but always produces an infinite
 -- distribution for non-trivial events.
 conditional :: (a -> Bool) -> Dist a -> Dist a
-conditional event (Dist dist) = Dist table
-  where
-    table = flatten (matches : replacements)
-    matches = filter (event . snd) dist
-    unmatches = filter (not . event . snd) dist
-    replacements = [ map (first (* p)) table | (p, _) <- unmatches ]
+conditional event dist = do
+  x <- dist
+  if event x
+    then return x
+    else conditional event dist
 
 -- | Samples the probability distribution to produce a value.
 sample :: Dist a -> IO a
-sample dist = go (unDist dist) . toRational <$> randomRIO (0 :: Double, 1)
+sample dist = go (possibilities dist) . toRational <$> randomRIO (0 :: Double, 1)
   where
     go [] _ = error "Dist.sample: empty distribution"
     go [(_, x)] _ = x
