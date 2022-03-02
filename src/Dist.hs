cd4482acdb09b0dfe9bd00b8d966c42d43227a17
Chris Smith <cdsmith@gmail.com>
2022-03-01 20:20:45 -0500

    Dist = decision tree.  Add some known dists

--- src/Dist.hs
+++ src/Dist.hs
@@ -1,30 +1,43 @@
 {-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE TupleSections #-}
 
 module Dist
-  ( Probability,
+  ( -- * Types
+    Probability,
     Dist,
     simplify,
+    conditional,
+    finiteConditional,
+
+    -- * Common distributions
+    categorical,
     uniform,
+    geometric,
     bernoulli,
+    binomial,
+    negativeBinomial,
+    hypergeometric,
+
+    -- * Analysis
     probability,
     approxProbability,
     expectation,
     variance,
     stddev,
     possibilities,
-    finiteConditional,
-    conditional,
     sample,
   )
 where
 
 import Control.Applicative (liftA2)
 import Control.Monad (ap)
-import Data.Bifunctor (bimap)
+import Data.Bifunctor (first)
+import Data.Bool (bool)
 import qualified Data.Map as Map
+import qualified Data.PQueue.Prio.Max as PQ
+import Data.Ratio
 import Data.Tuple (swap)
 import System.Random (randomRIO)
-import qualified Data.PQueue.Prio.Max as PQ
 
 -- | Probabilities are represented as rational numbers so we can ensure they add
 -- to 1.
@@ -33,7 +46,7 @@ type Probability = Rational
 -- | A probability distribution of values.
 data Dist a
   = Certainly a
-  | Choice [(Probability, Dist a)]
+  | Choice Probability (Dist a) (Dist a)
   deriving (Functor, Show)
 
 instance Applicative Dist where
@@ -42,7 +55,7 @@ instance Applicative Dist where
 
 instance Monad Dist where
   Certainly x >>= f = f x
-  Choice xs >>= f = Choice $ map (fmap (>>= f)) xs
+  Choice p a b >>= f = Choice p (a >>= f) (b >>= f)
 
 instance Num a => Num (Dist a) where
   (+) = liftA2 (+)
@@ -63,19 +76,98 @@ instance Fractional a => Fractional (Dist a) where
 -- even distributions with finitely many outcomes, but infinitely many paths to
 -- reach those outcomes) will hang.
 simplify :: Ord a => Dist a -> Dist a
-simplify dist =
-  Choice $ fmap Certainly . swap <$> Map.toList (Map.fromListWith (+) (swap <$> possibilities dist))
+simplify =
+  categorical
+    . fmap swap
+    . Map.toList
+    . Map.fromListWith (+)
+    . fmap swap
+    . possibilities
+
+-- | Produces the conditional probability distribution, assuming some event.
+-- This function works for all distributions, but always produces an infinite
+-- distribution for non-trivial events.
+conditional :: (a -> Bool) -> Dist a -> Dist a
+conditional event dist = do
+  x <- dist
+  if event x
+    then return x
+    else conditional event dist
+
+-- | Produces the conditional probability distribution, assuming some event.
+--
+-- This only works for finite distributions.  Infinite distributions (including
+-- even distributions with finitely many outcomes, but infinitely many paths to
+-- reach those outcomes) will hang.
+finiteConditional :: (a -> Bool) -> Dist a -> Dist a
+finiteConditional event dist = categorical (map (first (/ p_event)) filtered)
+  where
+    filtered = filter (event . snd) (possibilities dist)
+    p_event = sum (map fst filtered)
+
+-- | A distribution with a fixed probability for each outcome.  The
+-- probabilities should add to 1, but this is not checked.
+categorical :: [(Probability, a)] -> Dist a
+categorical = go 1
+  where
+    go _ [] = error "Empty distribution is not allowed"
+    go p ((q, x) : xs)
+      | null xs || q >= p = Certainly x
+      | otherwise = Choice (q / p) (Certainly x) (go (p - q) xs)
 
 -- | A uniform distribution over a list of values.
 uniform :: [a] -> Dist a
-uniform xs = Choice [(recip n, Certainly x) | x <- xs]
-  where
-    n = toRational (length xs)
+uniform xs = categorical $ (recip n,) <$> xs where n = toRational (length xs)
+
+-- | Geometric distribution over a list of possibilities.
+geometric :: Probability -> [a] -> Dist a
+geometric _ [] = error "geometric: Empty distribution is not allowed"
+geometric _ [x] = Certainly x
+geometric p (x : xs) = Choice p (Certainly x) (geometric p xs)
 
 -- | A Bernoulli distribution.  This gives True with probability @p@, and False
 -- otherwise.
 bernoulli :: Probability -> Dist Bool
-bernoulli p = Choice [(p, Certainly True), (1 - p, Certainly False)]
+bernoulli p = Choice p (Certainly True) (Certainly False)
+
+-- | Computes nCk.  This is a building block for several well-known discrete
+-- distributions.
+choose :: Integer -> Integer -> Integer
+n `choose` k
+  | k > n `div` 2 = n `choose` (n - k)
+  | otherwise = product [n - k + 1 .. n] `div` product [1 .. k]
+
+-- | A binomial distribution.  This gives the distribution of number of
+-- successes in @n@ trials with probability @p@ of success.
+binomial :: Integer -> Probability -> Dist Integer
+binomial n p =
+  categorical
+    [ (fromInteger (n `choose` k) * p ^ k * (1 - p) ^ (n - k), k)
+      | k <- [0 .. n]
+    ]
+
+-- | Negative binomial distribution.  This gives the distribution of number of
+-- failures before @r@ successes with probability @p@ of success.
+negativeBinomial :: Integer -> Probability -> Dist Integer
+negativeBinomial 0 _ = pure 0
+negativeBinomial r p =
+  categorical
+    [ (fromInteger ((k + r - 1) `choose` (r - 1)) * p ^ r * (1 - p) ^ k, k)
+      | k <- [0 ..]
+    ]
+
+-- | Hypergeometric distribution.  This gives the distribution of number of
+-- successful draws out of @n@ attempts without replacement, when @k@
+-- possibilities are successful.
+hypergeometric :: Integer -> Integer -> Integer -> Dist Integer
+hypergeometric n pop k =
+  categorical
+    [ ((k `choose` m) * ((pop - k) `choose` (n - m)) % (pop `choose` n), m)
+      | m <- [lo .. hi]
+    ]
+  where
+    lo = max 0 (n + k - pop)
+    hi = min n k
 
 -- | Computes the probability of an event, represented by a predicate on values.
 --
@@ -83,7 +175,9 @@ bernoulli p = Choice [(p, Certainly True), (1 - p, Certainly False)]
 -- even distributions with finitely many outcomes, but infinitely many paths to
 -- reach those outcomes) will hang.
 probability :: (a -> Bool) -> Dist a -> Probability
-probability event = sum . fmap fst . filter (event . snd) . possibilities
+probability event (Certainly x) = if event x then 1 else 0
+probability event (Choice p a b) =
+  p * probability event a + (1 - p) * probability event b
 
 -- | Like probability, but produces a lazy list of ever-improving ranges of
 -- probabilities.  This can be used on infinite distributions, for which the
@@ -102,7 +196,9 @@ approxProbability event = go 0 1 . possibilities
 -- even distributions with finitely many outcomes, but infinitely many paths to
 -- reach those outcomes) will hang.
 expectation :: Fractional a => Dist a -> a
-expectation dist = sum [ fromRational p * x | (p, x) <- possibilities dist ]
+expectation (Certainly x) = x
+expectation (Choice p a b) =
+  fromRational p * expectation a + fromRational (1 - p) * expectation b
 
 -- | Computes the variance of a finite distribution.
 --
@@ -111,7 +207,8 @@ expectation dist = sum [ fromRational p * x | (p, x) <- possibilities dist ]
 -- reach those outcomes) will hang.
 variance :: (Real a, Fractional a) => Dist a -> a
 variance dist = expectation ((^ (2 :: Int)) . subtract mean <$> dist)
-  where mean = realToFrac (expectation dist)
+  where
+    mean = realToFrac (expectation dist)
 
 -- | Computes the standard deviation of a finite distribution.
 --
@@ -125,42 +222,19 @@ stddev dist = sqrt (variance dist)
 -- This will often contain duplicate values, which can be removed using 'nub',
 -- 'Data.Set.fromList', etc.
 possibilities :: Dist a -> [(Probability, a)]
-possibilities dist = go (PQ.singleton 1 dist)
+possibilities = step . PQ.singleton 1
   where
-    go queue
+    step queue
       | PQ.null queue = []
       | otherwise = case PQ.deleteFindMax queue of
-        ((p, Certainly x), queue') -> (p, x) : go queue'
-        ((p, Choice xs), queue') ->
-          go (foldr (\(q, x) -> PQ.insert (p * q) x) queue' xs)
+        ((p, dist), queue') -> handle p dist queue'
 
--- | Produces the conditional probability distribution, assuming some event.
---
--- This only works for finite distributions.  Infinite distributions (including
--- even distributions with finitely many outcomes, but infinitely many paths to
--- reach those outcomes) will hang.
-finiteConditional :: (a -> Bool) -> Dist a -> Dist a
-finiteConditional event dist = Choice (map (bimap (/ p_event) Certainly) filtered)
-  where
-    filtered = filter (event . snd) (possibilities dist)
-    p_event = sum (map fst filtered)
-
--- | Produces the conditional probability distribution, assuming some event.
--- This function works for all distributions, but always produces an infinite
--- distribution for non-trivial events.
-conditional :: (a -> Bool) -> Dist a -> Dist a
-conditional event dist = do
-  x <- dist
-  if event x
-    then return x
-    else conditional event dist
+    handle p (Certainly x) queue = (p, x) : step queue
+    handle p (Choice q a b) queue =
+      step $ PQ.insert (p * q) a $ PQ.insert (p * (1 - q)) b queue
 
 -- | Samples the probability distribution to produce a value.
 sample :: Dist a -> IO a
-sample dist = go (possibilities dist) . toRational <$> randomRIO (0 :: Double, 1)
-  where
-    go [] _ = error "Dist.sample: empty distribution"
-    go [(_, x)] _ = x
-    go ((p, x) : xs) k
-      | k < p = x
-      | otherwise = go xs (k - p)
+sample (Certainly x) = return x
+sample (Choice p a b) =
+  bool (sample b) (sample a) . (< p) . toRational =<< randomRIO (0 :: Double, 1)
