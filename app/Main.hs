980888344f0a82168a46ee480ca37d8dc6cf3bf0
Chris Smith <cdsmith@gmail.com>
2022-03-07 18:46:07 -0500

    Remove obsolete code from Main

--- app/Main.hs
+++ app/Main.hs
@@ -1,18 +1,4 @@
-{-# OPTIONS_GHC -Wno-type-defaults #-}
-
 module Main where
 
-import Data.Bifunctor (bimap)
-import Dice
-import Dist
-import System.IO (BufferMode (NoBuffering), hSetBuffering, stdout)
-
--- This is the D&D rule for rolling stats: roll 4d6 and drop the lowest.
-
 main :: IO ()
-main = do
-  hSetBuffering stdout NoBuffering
-  print $
-    map (bimap fromRational fromRational) $
-      take 1000 $
-        approxProbability (> 5) $ finiteConditional (> 2) (total (1 `d` 6))
+main = return ()
